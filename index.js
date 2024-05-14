6df120a773b70ae8ed1016780b13e11023645112
SorayaKarimyar <110332324+SorayaKarimyar@users.noreply.github.com>
2024-05-14 16:12:15 +0100

    add the basic search engine no API

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-function searchforminput(event){
+
+
+
+
+
+function searchforminput(event) {
     event.preventDefault();
     let inputform = document.querySelector("#search-form-input");
-    let cityvalue = document.querySelector("#city");
-    cityvalue.innerHTML=inputform.value;
-
-}
-
-
-
-
-let searchformElement = document.querySelector("#search-form");
-searchformElement.addEventListener("submit",searchforminput);
+    let cityvalue = document.querySelector(".weather-app-city");
+    cityvalue.innerHTML = inputform.value;
+    
+  }
+  
+  let searchformElement = document.querySelector("#search-form");
+  searchformElement.addEventListener("submit", searchforminput);
