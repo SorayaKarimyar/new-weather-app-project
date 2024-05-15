da68abb92db8de07d7765703b02db34ce7f9c907
SorayaKarimyar <110332324+SorayaKarimyar@users.noreply.github.com>
2024-05-15 11:13:22 +0100

    api integration

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+function refreshWeather(response){
 
+    let temperatureElement= document.querySelector(".weather-app-temperature");
+    let temperatureof = response.data.temperature.current;
+    temperatureElement.innerHTML= Math.round(temperatureof);
+}
+
+   
+
+function searchCity(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+  }
 
 
 
@@ -8,6 +21,8 @@ function searchforminput(event) {
     let inputform = document.querySelector("#search-form-input");
     let cityvalue = document.querySelector(".weather-app-city");
     cityvalue.innerHTML = inputform.value;
+    searchCity(inputform.value);
+
     
   }
   
